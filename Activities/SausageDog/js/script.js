a487756cb71252179eae13a2125d35bfafb3975a
Wacqo <61024934+Wacqo@users.noreply.github.com>
2021-01-26 11:23:25 -0500

    A1: this is where the activity is getting all set up

---
+++ Activities/SausageDog/js/script.js
@@ -0,0 +1,17 @@
+/**************************************************
+Sausage Dog
+Nico Brinton
+
+Here is a description of this template p5 project.
+**************************************************/
+// setup()
+//
+// Description of setup() goes here.
+
+function setup() {
+  createCanvas(1000, 500);
+}
+
+function draw() {
+  background(0);
+}
---
+++ SausageDog/js/script.js
@@ -0,0 +1,18 @@
+/**************************************************
+Sausage Dog
+Nico Brinton
+
+Here is a description of this template p5 project.
+**************************************************/
+// setup()
+//
+// Description of setup() goes here.
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0);
+}
+     
