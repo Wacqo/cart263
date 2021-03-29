887c73ec88cf442fd7cc3e324cddc377ce243e74
Wacqo <61024934+Wacqo@users.noreply.github.com>
2021-03-29 15:01:27 -0400

    this is finished with the tutorial

--- Activities/ code-taker/js/script.js
+++ Activities/ code-taker/js/script.js
@@ -1,11 +1,40 @@
 /**
-Title of Project
-Author Name
+Code taker
+Nico brinton
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+
 */
 
 "use strict";
 
-// Code goes here
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function(){
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+$(`.secret`).draggable({
+  helper: `clone`
+});
+});
+
+
+$(`#answer`).droppable({
+  drop: function(event, ui){
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+
+    //check if they got it right
+    if ($(this).text() === `Theremin`){
+        $(`#solved-dialog`).dialog(`open`);
+    }
+  }
+});
